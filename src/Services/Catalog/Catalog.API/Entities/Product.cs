25f4095de53f2f0e3537b1648ac8022a682f1653
fad16papa <fad16papa@gmail.com>
2021-05-24 23:44:40 +0800

    Commit Added Product Entity

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -1,3 +1,4 @@
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace src.Services.Catalog.Catalog.API.Entities
@@ -5,6 +6,14 @@ namespace src.Services.Catalog.Catalog.API.Entities
     public class Product
     {
         [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public int Id { get; set; }
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
     }
 }
