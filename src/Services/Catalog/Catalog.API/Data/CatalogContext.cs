58fb257d97657a2e6c93d108476da753c2aa4136
fad16papa <fad16papa@gmail.com>
2021-05-25 00:03:40 +0800

    Commit changes

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,16 @@
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogInterface
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+
+        public IMongoCollection<Product> Products => throw new System.NotImplementedException();
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MongoDB.Driver;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogInterface
+    {
+       IMongoCollection<Product> Products { get; }
+    }
+}
